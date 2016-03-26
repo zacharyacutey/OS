31384955c91c4da2d91f306261026a7206096c1a
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-25 22:21:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-
+var fs={};
+function DirectoryExists(dir)
+{
+  if(dir in fs)
+  {
+    return fs[dir]===0;
+  }
+  return false;
+}
+function DirectoryCanMake(dir) 
+{
+  var t=dir.split("/");
+  if(t.length==1) return true;
+  return DirectoryExists(t.slice(0,t.length-1).join("/"))
+}
+function MakeDirectory(dir)
+{
+  if(!DirectoryCanMake(dir))
+  {
+    throw new Error("ERROR!");
+  }
+  fs[dir]=0;
+}
