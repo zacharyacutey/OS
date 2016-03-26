a8f631d52c828b22b8607d784b5a92b8481a358b
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-26 12:40:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ Array.prototype.append=function(item){
    this[this.length]=item;
   }
 };
+Array.prototype.append_=function(item){this[this.length]=item;};
 
 function WriteToFile(name,text)
 {
@@ -30,4 +31,39 @@ function AppendToFile(name,text)
 function DeleteFile(name)
 {
   fs.splice(fs.map(function(arg){return arg[0];}).indexOf(name) , 1);
+}
+
+function WriteToUI(text)
+{
+  document.getElementById("ui").innerHTML=text;
+}
+
+function SaveFileEvalString(name)
+{
+  return "WriteToFile('"+name"','"+GetFileText(name)+"');";
+}
+
+function SaveEvalString()
+{
+  var r="";
+  for(var i in fs)
+  {
+    r+=SaveFileEvalString(i);
+  }
+  return r;
+}
+
+function CookieString()
+{
+  return 'eval="'+SaveEvalString()+'";';
+}
+
+function Save()
+{
+  document.cookie="expires=Wed, 1 Jan 2020 00:00:00 EST;"+CookieString();
+}
+
+function Load()
+{
+  eval(document.cookie.substring(37,document.cookie.length-2));
 }
