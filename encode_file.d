3eaae72bb6904950d0ebd542c2d5674bdfc8f004
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-07-04 12:43:01 -0400

    Create encode_file.d

---
+++ encode_file.d
@@ -0,0 +1,30 @@
+module encode_file;
+string ContentEncodeString(dstring arg)
+{
+	import std.conv;
+	int[] temp;
+	int i = 0;
+	while(i != arg.length)
+	{
+		temp~=arg[i];
+		i++;
+	}
+	return text(temp);
+}
+dstring ContentDencodeString(string arg)
+{
+	import std.conv;
+	string refd = arg;
+	int[] temp = parse!(int[])(refd);
+	dchar[] s;
+	int i;
+	while(i!=temp.length)
+	{
+		s~=temp[i];
+		i++;
+	}
+	return s.idup;
+}
+void main()
+{
+}
