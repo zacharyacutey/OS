6fef83af3e34eccd7ededc796633dababb5826e5
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-07-08 13:14:03 -0400

    Update shell.d

--- shell.d
+++ shell.d
@@ -1,4 +1,25 @@
 module shell;
-string shell(string[] args)
+string[string] vars;
+string shell(string[] cmd)
 {
+  import std.stdio;
+  string command = cmd[0];
+  string[] args = cmd[1 .. $];
+  /*
+  Sigh, this is going to be a bunch of conditionals
+  */
+  if(command == "write")
+  {
+    write(args[0]);
+    return args[0];
+  }
+  else if(command == "writeln")
+  {
+    writeln(args[0]);
+    return args[0];
+  }
+  else if(command == "read")
+  {
+    return readln()[0 .. $ - 1];
+  }
 }
